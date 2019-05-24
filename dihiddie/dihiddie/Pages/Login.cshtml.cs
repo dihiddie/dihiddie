968bfae8b70755da9234fb06b9326c02fd455a29
dihiddie <zagrtd@gmail.com>
2019-05-24 17:16:44 +0300

    login

--- dihiddie/dihiddie/Pages/Login.cshtml.cs
+++ dihiddie/dihiddie/Pages/Login.cshtml.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dihiddie.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dihiddie.Pages
 {
-    public class PrivacyModel : PageModel
+    public class LoginModel : PageModel
     {
+        [BindProperty]
+        public User Admin { get; set; }
+
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPostLogin()
+        {
+            return RedirectToPage("./Stories");
         }
     }
 }
