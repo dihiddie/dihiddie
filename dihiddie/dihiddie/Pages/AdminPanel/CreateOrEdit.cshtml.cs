8f14c71a859dc4ab6fbb3aeb8e8db0c0d22bc465
Unknown <dihiddie@mail.ru>
2019-06-18 22:20:45 +0300

    create or edit with summernote

---
+++ dihiddie/dihiddie/Pages/AdminPanel/CreateOrEdit.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace dihiddie.Pages.AdminPanel
+{
+    public class CreateOrEditModel : PageModel
+    {
+        public SelectList EntryList { get; set; }
+
+        public CreateOrEditModel()
+        {
+            EntryList = new SelectList(new string[] { "Блог","Дневник" });
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
