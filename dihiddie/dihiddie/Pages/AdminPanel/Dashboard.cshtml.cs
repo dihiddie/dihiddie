7cbc50f3906b217ada671f030186514ec85e4cfd
dihiddie <dihiddie@mail.ru>
2020-01-16 23:31:11 +0300

    posts list for edit

--- dihiddie/dihiddie/Pages/AdminPanel/Dashboard.cshtml.cs
+++ dihiddie/dihiddie/Pages/AdminPanel/Dashboard.cshtml.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dihiddie.DAL.Post.Core.Models;
+using dihiddie.DAL.Post.Core.UnitOfWorks;
 using dihiddie.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dihiddie.Pages.AdminPanel
 {
     public class Dashboard : PageModel
     {
-        public IActionResult OnGet()
+        private IPostUnitOfWork unitOfWork;
+
+        public Dashboard(IPostUnitOfWork unitOfWork) => this.unitOfWork = unitOfWork;
+
+        public List<PostInformation> Posts { get; set; }
+
+        public async Task OnGetAsync()
         {
-            return UserHelper.ProcessAuthorized();
+            UserHelper.ProcessAuthorized();
+            var posts = await unitOfWork.PostRepository.GetPreviewsAsync().ConfigureAwait(false);
+            Posts = posts.ToList();
         }
     }
 }
