fa40c448a15b76435bdefae44662dfe1403a65ec
dihiddie <dihiddie@mail.ru>
2021-02-04 22:36:38 +0300

    scdfv

--- dihiddie/Utils/dihiddie.Utils.GoogleBlogpostsLoader/Program.cs
+++ dihiddie/Utils/dihiddie.Utils.GoogleBlogpostsLoader/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace dihiddie.Utils.GoogleBlogpostsLoader
 {
+    using System.Collections.Generic;
     using System.IO;
     using System.Text;
-
+    using AutoMapper;
     using dihiddie.DAL.Post.Core.Models;
     using dihiddie.DAL.Post.Core.Repositories;
+    using dihiddie.DAL.Post.EF.AutomapperProfiles;
+    using dihiddie.DAL.Post.EF.Context;
     using dihiddie.DAL.Post.EF.Repositories;
     using dihiddie.Utils.GoogleBlogpostsLoader.Models;
 
@@ -16,22 +19,31 @@
     {
         private static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection().AddSingleton<IPostRepository, PostRepository>()
+            var serviceProvider = new ServiceCollection().AddSingleton<IPostRepository, PostRepository>().AddSingleton<DihiddieContext>()                
                 .BuildServiceProvider();
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new PostProfile());
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            serviceProvider.AddAutoMapper(x => x.(new PostProfile))
 
             var postRepo = serviceProvider.GetService<IPostRepository>();
 
-            var path = @"C:\work\personal\dihiddie\dihiddie\Utils\dihiddie.Utils.GoogleBlogpostsLoader\blogposts.json";
+            var path = @"D:\work\dihiddie\dihiddie\Utils\dihiddie.Utils.GoogleBlogpostsLoader\blogposts.json";
             var responseBodyPostsInfo = File.ReadAllText(path);
             var des = JsonConvert.DeserializeObject<BlogPostInfo>(responseBodyPostsInfo);
 
             foreach (var blogPost in des.Items)
             {
-                var content = new PostContent { Content = Encoding.UTF8.GetBytes(blogPost.content), Title = blogPost.title };
+                var content = new DAL.Post.Core.Models.PostContent { Content = Encoding.UTF8.GetBytes(blogPost.content), Title = blogPost.title };
                 postRepo.SaveContentAsync(content).GetAwaiter().GetResult();
 
-                var info = new PostInformation
-                               {
+
+                var info = new DAL.Post.Core.Models.PostInformation
+                {
                                    CreateDateTime = blogPost.published,
                                    IsBlogPost = true,
                                    PostContentId = content.Id,
@@ -39,7 +51,7 @@
                                    Title = content.Title,
                                    UpdateDateTime = blogPost.updated
                                };
-                postRepo.SaveAsync(info);
+                postRepo.SaveAsync(info).GetAwaiter().GetResult();
             }
         }
     }
